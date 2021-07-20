6b6c599434ce8f11779a67678a186fe92e424876
CarlosAraki <carlosarak@gmail.com>
2021-07-20 00:53:26 +0000

    feat: include run node ,js

--- main.js
+++ main.js
@@ -20,4 +20,19 @@ chain.addNewBlock(
   })
 );
 
+const express = require('express');
+
+// Constants
+const PORT = 8080;
+const HOST = '0.0.0.0';
+
+// App
+const app = express();
+app.get('/', (req, res) => {
+  res.send('Hello World');
+});
+
+app.listen(PORT, HOST);
+console.log(`Running on http://${HOST}:${PORT}`);
+
 console.log(JSON.stringify(chain, null, 4));
